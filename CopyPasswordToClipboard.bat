81ab3eaf5510d48b1ec85d1bbb6ae046bab1802b
hsuehyt <hsueh.yuting@gmail.com>
2024-09-29 10:26:56 +0800

    Update CopyPasswordToClipboard.bat

--- CopyPasswordToClipboard.bat
+++ CopyPasswordToClipboard.bat
@@ -2,13 +2,13 @@
 setlocal
 
 :: Path to the text file (relative path to subfolder)
-set file_path=%~dp0TNUAcopySource\TNUAEmail.txt
+set file_path=%~dp0Folder\\Email.txt
 
 :: Read the content of the text file
-set content=
-for /f "delims=" %%i in (%file_path%) do set content=%%i
+set "content="
+for /f "delims=" %%i in (%file_path%) do set "content=%%i"
 
-:: Use PowerShell to copy the content to the clipboard
-echo %content% | powershell -command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.Clipboard]::SetText([Console]::In.ReadToEnd())"
+:: Use PowerShell to copy the content to the clipboard without extra space
+powershell -command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.Clipboard]::SetText('%content%')"
 
 endlocal
